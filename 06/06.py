3f31a9c29255383cde0c21de24a00d479633fba6
Patrick Kellar <pjkellar@gmail.com>
2020-12-18 17:46:04 -0500

    Updated p1 to better prepare for p2

--- 06/06.py
+++ 06/06.py
@@ -1,22 +1,31 @@
+import re
+
 with open('data.txt') as file:
     data = file.readlines()
     data = [line.strip() for line in data]
 
-answers = ''
+answers = []
 groupAnswers = []
 
 for line in data:
     if line != '':
-        answers += line
+        answers.append(line)
     else:
         groupAnswers.append(answers)
-        answers = ''
+        answers = []
+
 #catch the last set of answers
 groupAnswers.append(answers)
 
-totalQuestions = 0
-for answers in groupAnswers:
-    uniqueChars = set(answers)
-    totalQuestions += int(len(uniqueChars))
+p1 = 0
+p2 = 0
 
-print(totalQuestions)
+for answers in groupAnswers:
+    charString = ''
+    for person in answers:
+        charString += person 
+    uniqueChars = set(charString)
+    p1 += len(uniqueChars)
+
+print(p1)
+print(p2)
